01850b72a838005e8c08ef4d2bfd6c3831ab786e
Bubeos <2534056@etu.cchic.ca>
2025-11-20 18:54:20 -0500

    okkk

--- Projet-2-DevApp-develop/GameOn/App.xaml.cs
+++ Projet-2-DevApp-develop/GameOn/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using AutoUpdaterDotNET;
 
 namespace GameOn
 {
@@ -9,6 +10,13 @@ namespace GameOn
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Lien RAW GitHub
+            AutoUpdater.Start("https://raw.githubusercontent.com/Boualem234/DeploiementSAB/main/update/update.xml");
+
+            base.OnStartup(e);
+        }
     }
 
 }
